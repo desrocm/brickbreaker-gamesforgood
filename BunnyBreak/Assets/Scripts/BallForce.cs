bb75c72989a44ff75b935c9e1cc457ac13bdeb3c
Michelle Desrochers <mdesrochers08@gmail.com>
2020-04-09 16:22:48 -0400

    Made this a while ago

---
+++ BunnyBreak/Assets/Scripts/BallForce.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BallForce : MonoBehaviour
+{
+    public Rigidbody2D rb; 
+    public float ballForce;
+    bool gameStarted = false;
+    public GameObject startbutton;
+    public Transform paddle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void ReleaseBall()
+    {
+        transform.SetParent(null);
+        rb.isKinematic = false;
+        rb.AddForce(new Vector2(ballForce, ballForce));
+        gameStarted = true;
+        Destroy(startbutton);
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "ground")
+        {
+            SceneManager.LoadScene(0);
+            
+        }
+    }
+}
---
+++ BunnyBreak/Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour
+{
+    public float moveSpeed;
+    public GameObject paddle;
+
+    private Rigidbody2D paddleBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleBody = paddle.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PaddleLeft()
+    {
+        paddleBody.velocity = new Vector2(-moveSpeed, paddleBody.velocity.y);
+    }
+    public void PaddleRight()
+    {
+        paddleBody.velocity = new Vector2(moveSpeed, paddleBody.velocity.y);
+    }
+    public void PaddleStop()
+    {
+        paddleBody.velocity = new Vector2(0f, paddleBody.velocity.y);
+    }
+}
